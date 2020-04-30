1bbfab6085e23c63e866af7e8ef2e456d6b43452
Ian Tuan A <4982030-ianvu@users.noreply.gitlab.com>
2020-04-30 19:22:19 +0200

    :lipstick: multiplayer

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const server = express()
 const io = socketIO(server);
 
 let users = [];
+let currentPlayer = null;
+let timeout = null;
+const words = ['Apple', 'Cherry', 'Banane', 'Orange', 'Kiwi',]
 
 io.on('connection', (socket) => {
     console.log('A new user joined the game');
@@ -24,7 +27,16 @@ function onConnection(socket) {
     socket.on('username', username => {
         console.log('Player name:', username);
         socket.username = username;
-        users.push(socket);
+
+        if (users.length === 0) {
+            currentPlayer = socket;
+            users.push(socket);
+            switchPlayer();
+        } else {
+            users.push(socket);
+            sendUsers();
+        }
+
         sendUsers();
     });
 
@@ -34,6 +46,10 @@ function onConnection(socket) {
             return user !== socket;
         });
         sendUsers();
+
+        if (users.length ===0) {
+            timeout = clearTimeout(timeout);
+        }
     });
 
     socket.on('line', data => {
@@ -43,6 +59,23 @@ function onConnection(socket) {
 
 function sendUsers () {
     io.emit('users', users.map(user => {
-        return user.username;
+        return {
+            username: user.username,
+            isActive: currentPlayer === user
+        }
     }));
+}
+
+function switchPlayer () {
+
+    const indexCurrentPlayer = users.indexOf(currentPlayer);
+    currentPlayer = users[(indexCurrentPlayer + 1) % users.length];
+    
+    sendUsers();
+
+    const newWord = words[Math.floor(Math.random() * words.length)];
+    currentPlayer.emit('word', newWord);
+    io.emit('clear');
+
+    timeout = setTimeout(switchPlayer, 50000);
 }
